bfe15395514e6e876c3430c994eacc9bf9fd7034
ytj <3287816638@qq.com>
2021-03-28 00:21:44 +0800

    box

--- active/random_walk.py
+++ active/random_walk.py
@@ -46,23 +46,51 @@ class RandomWalkerScene2(Scene):
         walker_group.set_color(BLUE)
         #walker_group.set_submobject_colors_by_gradient(BLUE, RED)
         colors = color_gradient([BLUE, ORANGE, RED], 101)
-        for i in range(int(self.run_time/self.dt)):
-            for sub in walker_group:
-                sub.factor += 0.005
-                sub.get_direction()
-                sub.set_color(colors[int(200 * sub.factor)])
-            self.play(*[ApplyMethod(sub.shift, sub.direction) for sub in walker_group],
-                      rate_func=self.rate_func,
-                      run_time=self.dt)
+        back_ground_colors = color_gradient([BLUE_A, BLUE_E], 101)
+        image = ImageMobject("sea.jpg")
+        self.add(image)
+        back_ground = self.container_box()
+        back_ground.set_fill(opacity=0.4)
+        self.add(back_ground)
+        self.wait()
+        # for i in range(int(self.run_time/self.dt)):
+        #     for sub in walker_group:
+        #         sub.factor += 0.005
+        #         sub.get_direction()
+        #         sub.set_color(colors[int(200 * sub.factor)])
+        #         back_ground.set_fill(back_ground_colors[int(200 * sub.factor)])
+        #     self.play(*[ApplyMethod(sub.shift, sub.direction) for sub in walker_group],
+        #               rate_func=self.rate_func,
+        #               run_time=self.dt)
+        #
+        # for i in range(int(self.run_time/self.dt)):
+        #     for sub in walker_group:
+        #         sub.factor -= 0.005
+        #         sub.get_direction()
+        #         sub.set_color(colors[int(200 * sub.factor)])
+        #         back_ground.set_fill(back_ground_colors[int(200 * sub.factor)])
+        #     self.play(*[ApplyMethod(sub.shift, sub.direction) for sub in walker_group],
+        #               rate_func=self.rate_func,
+        #               run_time=self.dt)
 
-        for i in range(int(self.run_time/self.dt)):
-            for sub in walker_group:
-                sub.factor -= 0.005
-                sub.get_direction()
-                sub.set_color(colors[int(200 * sub.factor)])
-            self.play(*[ApplyMethod(sub.shift, sub.direction) for sub in walker_group],
-                      rate_func=self.rate_func,
-                      run_time=self.dt)
+    def container_box(self):
+        inside = Polygon(np.array([4.0, 2.0, 0.0]),
+                              np.array([4.0, -2.0, 0.0]),
+                              np.array([-4.0, -2.0, 0.0]),
+                              np.array([-4.0, 2.0, 0.0]))
+        vertices = inside.get_vertices()
+
+#TODO 黄金分割比的容器且自动生成
+        width = self.camera.get_frame_width()/2
+        height = self.camera.get_frame_height()/2
+        rightside = Polygon(np.array([4.0, 2.0, 0.0]), np.array([2.0, -1.0, 0.0]), RIGHT * width + DOWN * height, RIGHT * width + UP * height)
+        downside = Polygon(np.array([4.0, -2.0, 0.0]), np.array([-2.0, -1.0, 0.0]), LEFT * width + DOWN * height,
+                         RIGHT * width + DOWN * height)
+        leftside = Polygon(np.array([-4.0, -2.0, 0.0]), np.array([-2.0, 1.0, 0.0]), LEFT * width + UP * height,
+                         LEFT * width + DOWN * height)
+        upside = Polygon(np.array([-4.0, 2.0, 0.0]), np.array([2.0, 1.0, 0.0]), RIGHT * width + UP * height,
+                         LEFT * width + UP * height)
+        return VGroup(inside, upside, rightside, downside, leftside)
 
 class RandomWalkerScene3(RandomWalkerScene2):
     CONFIG = {
